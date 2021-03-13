0f402d3f1d927129e44b602f756e8ebce6b24d72
Prasath Ravichandran <48789364+PrasathRavichandran@users.noreply.github.com>
2021-03-13 10:51:56 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmFaFTco9j9eq3RaiVsKsW7CwdHE6MZ3M",
-  authDomain: "chat-app-vue-f11b0.firebaseapp.com",
-  projectId: "chat-app-vue-f11b0",
-  storageBucket: "chat-app-vue-f11b0.appspot.com",
-  messagingSenderId: "340566119240",
-  appId: "1:340566119240:web:c7a6d11bb76b73ff4cd97d",
-  measurementId: "G-MM0SMJ6EQZ",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
